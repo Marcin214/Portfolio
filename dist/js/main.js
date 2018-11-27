0b8f8f86281ee10b8b79dec70e674824b6301226
marcin <marcin.grand21@gmail.com>
2018-11-27 22:22:21 +0100

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,11 @@
-//Select DOM Items
-
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
-//Set Initial State Of Menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -19,7 +18,7 @@ function toggleMenu() {
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
-        //Set Menu State
+        // Set Menu State
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
@@ -28,8 +27,7 @@ function toggleMenu() {
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
-        //Set Menu State
+        // Set Menu State
         showMenu = false;
-
     }
 }
